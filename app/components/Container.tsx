0492f2fe79e8e3426a3c8b2bb02124dd58ed05e0
Monowar Hussain <monowargtp@gmail.com>
2023-06-03 16:37:09 +0530

    initial

---
+++ app/components/Container.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+interface containerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<containerProps> = ({children}) => {
+  return (
+    <div className="max-w-[2520px] mx-auto xl:px-20 md:px10 sm:px-2 px-4">
+      {children}
+    </div>
+  )
+}
+
+export default Container
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import {Nunito} from 'next/font/google';
+import './globals.css'
+import Navbar from './components/Navbar/Navbar';
+
+export const metadata = {
+  title: 'PixTravel',
+  description: 'An Airbnb Clone',
+}
+
+const font = Nunito({
+  subsets:["latin"],
+});
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      
+      <body className={font.className}>
+      <Navbar />
+        {children}
+        </body>
+    </html>
+  )
+}
